0e50410d190f680427e141af46e63bdef1e9b53c
马如君(Marrick Jasper Ma) <Marrick2005@outlook.com>
2026-02-27 23:21:35 +0800

    Change database password in database.py
    Updated database connection password for security.

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 数据库连接 URL
 import urllib.parse
-password = urllib.parse.quote_plus("210828@Max")
+password = urllib.parse.quote_plus("yourpassword")
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:{password}@localhost/smart_village_db?charset=utf8mb4"
 
 engine = create_engine(
